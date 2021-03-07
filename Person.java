e11d1d8d0d399805a0427a77aae0960079b0afe2
Christian S <chris.salapang@gmail.com>
2021-03-07 23:48:47 +0800

    added get and set

--- Person.java
+++ Person.java
@@ -1,4 +1,19 @@
 public class Person {
     public String firstname;
     public String lastname;
+
+    public String getFirstname(){
+        return firstname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setFirstname(String setfn) {
+        firstname = setfn;
+    }
+    public void setLastname(String setln) {
+        lastname = setln;
+    }
+
 }
